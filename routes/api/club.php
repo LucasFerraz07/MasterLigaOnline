d56da06261e114720baf13eea00e2b39a9d2c33f
LucasFerraz07 <lucas.ferrazm0411@gmail.com>
2026-07-04 18:24:34 -0300

    <Feat>[Club]:Create club route, controller and requests

---
+++ routes/api/club.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\Club\Club;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth.api', 'permission:club.view')->group(function () {
+    Route::get('/', [Club::class, 'index']);
+    Route::get('/{id}', [Club::class, 'show']);
+});
+
+Route::middleware('auth.api', 'permission:club.create')->group(function () {
+    Route::post('/', [Club::class, 'store']);
+});
+
+Route::middleware('auth.api', 'permission:club.update')->group(function () {
+    Route::put('/{id}', [Club::class, 'update']);
+});
+
+Route::middleware('auth.api', 'permission:club.delete')->group(function () {
+    Route::delete('/{id}', [Club::class, 'destroy']);
+});
--- routes/api/index.php
+++ routes/api/index.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->group(base_path('routes/api/auth.php'));
 Route::prefix('subscription')->group(base_path('routes/api/subscription.php'));
+Route::prefix('club')->group(base_path('routes/api/club.php'));
